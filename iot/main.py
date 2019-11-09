34d417fba62d624776021d24d7c8c6caf7600098
= <kevinjamesmann@gmail.com>
2019-11-09 14:58:00 +0900

    add history capability to firestore db

--- iot/main.py
+++ iot/main.py
@@ -1,5 +1,6 @@
 """This is the main program"""
 import time
+import requests
 
 import RPi.GPIO as GPIO
 
@@ -23,6 +24,9 @@ class Iot(object):
     database = None
     temperatureAndHumidityPin = 18
     bmp = BMP085(0x77)
+    geoloc_url = 'https://api.ipgeolocation.io/ipgeo'
+    geoloc_apikey='7f4599fb2cf546dab195e2158c984c42'
+    weather_url='https://api.darksky.net/forecast/ad81fa1aa56f70d51d00344adec8def2/'
 
     def __init__(self):
         # setup
@@ -42,10 +46,11 @@ class Iot(object):
         humidity, temperature = Adafruit_DHT.read_retry(11, self.temperatureAndHumidityPin)
         print(humidity)
         print(temperature)
-        if temperature is not None:
-            self.send_to_firestore('temperature', round(temperature, 2))
-        if humidity is not None:
-            self.send_to_firestore('humidity', round(humidity, 2))
+        #if temperature is not None:
+        #    self.send_to_firestore('temperature', round(temperature, 2))
+        #if humidity is not None:
+        #    self.send_to_firestore('humidity', round(humidity, 2))
+        return (temperature, humidity)
 
     def pressure_sensor(self):
         """This method gets the pressure value
@@ -57,10 +62,54 @@ class Iot(object):
 
         pressure = self.bmp.readPressure()
         print(pressure)
-        if pressure is not None:
-            self.send_to_firestore('pressure', int(round(pressure / 100.0, 2)))
+        #if pressure is not None:
+        #    self.send_to_firestore('pressure', int(round(pressure / 100.0, 2)))
+        return pressure
+    
+    def get_location(self):
+        geores = requests.get(self.geoloc_url, {'apiKey':self.geoloc_apikey})
+        if geores.status_code!=200:
+            raise Exception('failed to get location')
+        loc_dict = geores.json()
+        lat = loc_dict['latitude']
+        lon = loc_dict['longitude']
+        
+        return (lat,lon)
+        
+    def get_weather(self):
+        lat, lon = self.get_location()
+        url_w_params = self.weather_url+lat+','+lon
+        weather_res = requests.get(url_w_params, {'units':'si'})
+        if weather_res.status_code!=200:
+            print(weather_res.text())
+            raise Exception('failed to get weather')
+        
+        weather_dict = weather_res.json()
+        cur_weather_dict = weather_dict.get('currently', {'time':'','temperature':0,'pressure':0,'humidity':0})
+        # determine which weather items we want
+        cur_unixtime = cur_weather_dict['time']
+        cur_date = None
+        if cur_unixtime is not None:
+            cur_date = time.strftime("%Y-%m-%d %H:%M:%S (%Z)", time.localtime(cur_unixtime))
+        cur_temp = cur_weather_dict['temperature']
+        cur_press = cur_weather_dict['pressure']
+        cur_hum = cur_weather_dict['humidity']
+        # cur temp, cur air pressure, cur humidity
+        #if cur_temp is not None:
+        #    self.send_to_firestore('outside-temperature',cur_temp)
+        #if cur_press is not None:
+        #    self.send_to_firestore('outside_pressure',cur_press)
+        #if cur_hum is not None:
+        #    self.send_to_firestore('outside_humidity',cur_hum)
+        #if cur_date is not None:
+        #    self.send_to_firestore('date', cur_date)
+        print(cur_temp)
+        return cur_date, cur_temp, cur_press, cur_hum    
+        
+        
 
-    def send_to_firestore(self, collection, value):
+
+    def send_to_firestore(self, entry):
         """This method sends the values
            to the Firebase Firestore collection.
 
@@ -70,14 +119,11 @@ class Iot(object):
                 value (int): sensor value to be stored
         """
 
-        doc_ref = self.database.collection(collection).stream()
+        
 
         try:
-            for doc in doc_ref:
-                item = self.database.collection(collection).document(doc.id)
-                item.set({
-                    u'value': value
-                }, merge=True)
+            self.database.collection(u'data').add(entry)
+            print(entry)
         except Exception as exception:
             print(str(exception))
 
@@ -88,9 +134,34 @@ class Iot(object):
                 self (Context): Context reference.
         """
         try:
+            
             while True:
-                self.temperature_and_humidity_sensor()
-                self.pressure_sensor()
+                new_entry={
+                             'date':'',
+                             'temperature':0,
+                             'pressure':0,
+                             'humidity':0,
+                             'outdoor_temperature':0,
+                             'outdoor_pressure':0,
+                             'outdoor_humidity':0
+                           }
+                #TODO add in default return values for these
+                tmp_temp, tmp_humidity = self.temperature_and_humidity_sensor()
+                tmp_press = self.pressure_sensor()
+                try:
+                    tmp_date, tmp_otemp, tmp_opress, tmp_ohum = self.get_weather()
+                except:
+                    tmp_date, tmp_otemp, tmp_opress, tmp_ohum = ('', 0,0,0)
+                new_entry['date']=tmp_date
+                new_entry['temperature']=tmp_temp
+                new_entry['pressure']=tmp_press
+                new_entry['humidity']=tmp_humidity
+                new_entry['outdoor_temperature']=tmp_otemp
+                new_entry['outdoor_pressure']=tmp_opress
+                new_entry['outdoor_humidity']=tmp_ohum
+                
+                self.send_to_firestore(new_entry)
+                
                 time.sleep(self.interval)
 
         finally:
