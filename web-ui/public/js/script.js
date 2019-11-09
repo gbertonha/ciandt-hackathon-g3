f918320ab35f5a1709b9b2068edb85a0252616a4
Steppico <steppashr@gmail.com>
2019-11-09 12:31:58 +0900

    first commit

--- web-ui/public/js/script.js
+++ web-ui/public/js/script.js
@@ -1,6 +1,7 @@
 /*global firebase, document */
 /*jslint browser:true */
 "use strict";
+import axios from "axios";
 
 /**
  * Reads data from Firestore and updates information
@@ -8,30 +9,49 @@
  * @param {String} sensor The sensor key.
  */
 function readData(sensor) {
-    var db = firebase.firestore();
-    db.collection(sensor)
-        .onSnapshot(function (querySnapshot) {
-            querySnapshot.forEach(function (doc) {
-                document.getElementById(sensor).innerText = doc.data().value;
-                var today = new Date();
-                var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
-                var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-                var dateTime = date + ' ' + time;
-                document.getElementById("last-update").innerText = dateTime;
-            });
-        });
+  let db = firebase.firestore();
+  db.collection(sensor).onSnapshot(function(querySnapshot) {
+    querySnapshot.forEach(function(doc) {
+      document.getElementById(sensor).innerText = doc.data().value;
+      var today = new Date();
+      var date =
+        today.getFullYear() +
+        "-" +
+        (today.getMonth() + 1) +
+        "-" +
+        today.getDate();
+      var time =
+        today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+      var dateTime = date + " " + time;
+      document.getElementById("last-update").innerText = dateTime;
+    });
+  });
 }
 
 /**
  * Triggered once DOM is loaded.
  */
-document.addEventListener('DOMContentLoaded', function () {
-    try {
-        var sensors = ["temperature", "humidity", "pressure"];
-        sensors.forEach(function (sensor) {
-            readData(sensor);
-        });
-    } catch (e) {
-        console.error(e);
-    }
+document.addEventListener("DOMContentLoaded", function() {
+  try {
+    let sensors = ["temperature", "humidity", "pressure"];
+    sensors.forEach(function(sensor) {
+      readData(sensor);
+    });
+  } catch (e) {
+    console.error(e);
+  }
+});
+
+document.getElementsByClassName("get-my-ip")[0];
+
+document.addEventListener("click", async () => {
+  try {
+    const result = await axios({
+      method: "get",
+      url: "https://api.ipify.org?format=json"
+    });
+  } catch (e) {
+    console.log("error!", e);
+  }
+  console.log(result);
 });
